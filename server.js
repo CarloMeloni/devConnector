0409ac4791aa046d82e506b19d7104332f86f167
carloMeloni <carlo.meloni@hotmail.it>
2020-07-19 16:32:11 +0200

    Connecting to the database

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+//CONNECT DATABASE
+connectDB();
 
 app.get('/', (req, res) => res.send('API RUNNING MAN!'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log('Server started on ' + PORT));
+app.listen(PORT, () => console.log('Server started on ' + PORT));
 
