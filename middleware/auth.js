d5950c52d0c993e6ea77250a817a22e06f0093df
carloMeloni <carlo.meloni@hotmail.it>
2020-07-20 16:23:40 +0200

    Custom auth Middleware and JWT verify

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req, res, next) {
+    //GET TOKEN FROM THE HEADER
+    const token = req.header('x-auth-token');
+
+    //CHECK IF THERE IS NO TOKEN
+    if(!token) {
+        return res.status(401).json({ msg: 'No token, authorization denied man!' });
+    }
+
+    //VERIFY TOKEN 
+    try {
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+        req.user = decoded.user;
+        next();
+    } catch(err) {
+        res.status(401).json({ msg: 'Token is not valid man!' });
+    }
+}
