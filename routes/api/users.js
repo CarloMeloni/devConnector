50c179336556b036d3837b036affb0f8e469abe9
carloMeloni <carlo.meloni@hotmail.it>
2020-07-20 11:41:16 +0200

    request and body validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,20 @@
 const express = require('express');
-
 const router = express.Router();
+const {check, validationResult} = require('express-validator');
 
-//ROUTE api/user
-router.get('/', (req, res) => res.send('USER PAGE MAN!'));
+//ROUTE  POST api/user
+//REGISTER USER
+router.post('/', [
+    check('name', 'Name is required!').not().isEmpty(),
+    check('email', 'Your email is required!').isEmail().not().isEmpty(),
+    check('password', 'A password is required!').isLength({min: 6, max: 20}).not().isEmpty()
+], (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('USER PAGE MAN!');
+
+});
 
 module.exports = router;
