b32fe9fe8606850bcbc694818f64f4e7bb2548df
carloMeloni <carlo.meloni@hotmail.it>
2020-07-24 12:52:40 +0200

    Get ad Delete actions for posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -39,4 +39,92 @@ router.post('/', [auth, [
 
 });
 
+
+
+
+
+
+
+
+//ROUTE api/posts
+//GET ALL POSTS
+//PRIVATE
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+        res.send(500).send('Server error!')
+    }
+})
+
+
+
+
+
+
+
+
+//ROUTE api/posts/:id
+//GET POST BY ID
+//PRIVATE
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(!post) {
+            return res.status(404).json({ msg: 'Post not found!' })
+        }
+
+        res.json(post);
+    } catch (err) {
+        console.error(err.message);
+
+        if(err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found!' })
+        }
+
+        res.send(500).send('Server error!')
+    }
+})
+
+
+
+
+
+
+
+
+//ROUTE DELETE api/posts/:id
+//REMOVE A POST
+//PRIVATE
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if(!post) {
+            return res.status(404).json({ msg: 'Post not found!' })
+        }
+
+        //CHECK USER
+        if(post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized!' });
+        }
+
+        await post.remove();
+
+        res.json({ msg: 'Post is removed!' });
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found!' })
+        }
+        res.send(500).send('Server error!')
+    }
+})
+
+
+
+
 module.exports = router;
